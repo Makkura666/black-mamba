4f937944033562b997f5da966c57b1bbd6fd84a0
Jens Ackou <makkura666@hotmail.com>
2016-01-13 21:37:43 +0100

    Setup submodules

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 
+#include <cstdlib>
+
+void clear_screen()
+{
+#ifdef WINDOWS
+    std::system("cls");
+#else
+    // Assume POSIX
+    std::system ("clear");
+#endif
+}
+
 int main(int argc, const char *argv[]) 
 {
-    std::cout << "Engine init \n";
+    std::cout << R"###(
+     _ ) | __ _  _   | | _  | \   | __ _ _  _  | |   __ _
+ 	 _ \ |/ _` |/ _/ | |/ / | |\/ |/ _` |  ` \ | _ \/ _` |
+	___/_|\__,_|\__\ |_|\_\ |_|  _|\__,_|_|_|_||.__/\__,_|
+    )###";
+    std::cout << "\n              ### - Gaming Engine - ###\n";
+    clear_screen();
+    
 	return 0;
 }
