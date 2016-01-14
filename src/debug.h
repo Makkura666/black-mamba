d8f4e70f32299ad0f63c652371a04e997386357f
Jens Ackou <makkura666@hotmail.com>
2016-01-14 19:41:59 +0100

    GLFW init and window creation

--- src/debug.h
+++ src/debug.h
@@ -1,7 +1,13 @@
-clearScreen();
-std::cout << R"###(
- _ ) | __ _  _   | | _  | \   | __ _ _  _  | |   __ _
- _ \ |/ _` |/ _/ | |/ / | |\/ |/ _` |  ` \ | _ \/ _` |
-___/_|\__,_|\__\ |_|\_\ |_|  _|\__,_|_|_|_||.__/\__,_|
-)###";
-std::cout << "\n              ### - Gaming Engine - ###\n";
+if((int)glfwGetTime() == counter)
+{
+	clearScreen();
+	std::cout << R"###(
+	 _ ) | __ _  _   | | _  | \   | __ _ _  _  | |   __ _
+	 _ \ |/ _` |/ _/ | |/ / | |\/ |/ _` |  ` \ | _ \/ _` |
+	___/_|\__,_|\__\ |_|\_\ |_|  _|\__,_|_|_|_||.__/\__,_|
+	)###";
+	std::cout << "\n              ### - Gaming Engine - ###\n";
+	printf ("| Time | %*f \n", 5, glfwGetTime());
+	std::cout << "\n" << (int)glfwGetTime() << " " << counter;
+    counter++;
+}
